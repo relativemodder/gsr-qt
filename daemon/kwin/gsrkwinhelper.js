48546747926ed852ffeb814e5c24a9942b65eb2d
Andrew <kezin636@gmail.com>
2025-12-08 16:36:36 +0300

    more accurate title detection

--- daemon/kwin/gsrkwinhelper.js
+++ daemon/kwin/gsrkwinhelper.js
@@ -1,14 +1,38 @@
-
-workspace.windowActivated.connect((client) => {
-    print(client)
-    if (!client) return;
-
-    print(client.caption)
-    const title = client.caption || "";
-
+// call our daddy daemon to inform about new active window title
+function sendNewActiveWindowTitle(title) {
     callDBus(
         "com.github.relative.gsrqt", "/",
         "com.github.relative.gsrqt",
         "setActiveWindowTitle", title
     );
+}
+
+// don't track multiple windows' captions unnecessarily, map is ideal here
+const captionHandlers = new Map();
+
+
+function subToChanges(client) {
+    if (!client) return;
+    if (captionHandlers.has(client)) return; // already tracking
+
+    const handler = () => {
+        sendNewActiveWindowTitle(client.caption || "");
+    };
+
+    captionHandlers.set(client, handler);
+    client.captionChanged.connect(handler);
+}
+
+// on every focus
+workspace.windowActivated.connect((client) => {
+    if (!client) return;
+
+    sendNewActiveWindowTitle(client.caption || "");
+    subToChanges(client);
 });
+
+// on script load, send current active window title, just in case
+if (workspace.activeWindow) {
+    sendNewActiveWindowTitle(workspace.activeWindow.caption || "");
+    subToChanges(workspace.activeWindow);
+}
